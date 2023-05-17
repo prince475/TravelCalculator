aa46c475a1ed138ad9bf51ac431333a8aa4de219
Maureen <murugimaureen99@gmail.com>
2023-05-17 10:40:00 +0300

    Re-style benefits page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ClientPage from './Components/ClientPage';
 import Home from './Components/Home';
 import Calculator from './Components/Calculator';
 import Policy from './Components/Policy';
+import Benefits from './Components/Benefits';
 
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
     <Policy/>
     <hr className='border-sky-400 w-3/4 m-auto mb-4 mt-2'/>
     <Calculator/>
+    <hr className='border-sky-400 w-3/4 m-auto mb-4 mt-2'/>
+    <Benefits/>
     </div>
   );
 }
---
+++ src/Components/Benefits.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+
+function Benefits() {
+  return (
+    <div className='text-gray-600 font-body'>
+        <main className='px-16 py-6 body bg-white/80'>
+            <header>
+                <h2 className='text-black text-3xl font-semibold'>
+                    Benefits
+                </h2>
+            </header>
+
+            <div className='overflow-auto rounded-lg shadow'>
+                <table className='w-full'>
+                    <thead className='bg-gray-40 border-b-2 border-gray-400 text-xl'>
+                        <tr>
+                            <th className='p-3 text-sm font-semibold tracking-wide text-left'>Section of Cover</th>
+                            <th className='p-3 text-sm font-semibold tracking-wide text-left'>Max. Sum Insured</th>
+                            <th className='p-3 text-sm font-semibold tracking-wide text-left'>KES</th>
+                        </tr>
+                    </thead> 
+
+                    <tbody className='divide divide-gray-300'>
+                            <tr className=''>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    <span className='trow'>
+                                    Personal Accident(Death/PID)
+                                    </span>
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    USD 25,000
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    KES 3,400,000
+                                </td>
+                            </tr>
+
+                            <tr className=''>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    <span className='trow'>
+                                    Medical Expenses and Evacuation
+                                    </span>
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    USD 25,000
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    KES 3,400,000
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    <span className='trow'>
+                                    Emergency Dental Care
+                                    </span>
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    USD 25,000
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    KES 3,400,000
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    <span className='trow'>
+                                    Repatriation of Mortal Remains
+                                    </span>
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    USD 25,000
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    KES 3,400,000
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    <span className='trow'>
+                                    Hospital Benefits
+                                    </span>
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    USD 25,000
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    KES 3,400,000
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    <span className='trow'>
+                                    Loss of Checked Baggage
+                                    </span>
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    USD 25,000
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    KES 3,400,000
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    <span className='trow'>
+                                    Delayed Baggage
+                                    </span>
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    USD 25,000
+                                </td>
+                                <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                                    KES 3,400,000
+                                </td>
+                            </tr>
+                        </tbody>
+                </table>
+            </div>
+        </main>
+    </div>
+  )
+}
+
+export default Benefits
